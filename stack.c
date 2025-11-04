f437392b7e80afdbba12c36c15707e1db27e0184
VemuriChandrahaas <11249a404@kanchiuniv.ac.in>
2025-11-04 12:49:13 +0530

    Add stack implementation with push and display functions
    Implement basic stack operations including push and display.

---
+++ stack.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int TOP = -1, MAX;
+
+void display(int stack[])
+{
+    printf("current stack elements are - \n");
+    
+    for(int i=0; i <= TOP; i++)}
+    printf("(TOP)");
+}
+
+void push (int stack[]){
+    if(TOP == -1)
+    {
+        printf("top element in the stack : %d \n", stack[TOP]);
+        TOP = TOP - 1;
+    }
+    
+}
+
+int main()
+{
+  int operation=0, TOP = -1;
+  
+  printf("enter the size of stack:");
+  
+
+
+    
+while(operation !=3){
+        printf("\n 1.push \n 2.pop \n 3.exit \n");
+        printf("please choose stack operations to perform: ");
+        scanf("%d", &operation);
+        
+        switch(operation){
+            case 1:
+            push(STACK);
+            display(STACK);
+            break;
+            
+            case 2:
+            pop(STACK);
+            display(STACK);
+            break;
+            
+            case 3:
+             return 0;
+        }
+        return 0;
+    }
+
+}
